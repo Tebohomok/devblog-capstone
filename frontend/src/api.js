4dda8a69f64bd0d3b364264d519edb1ad0c866ab
Tebohomok <brateboho.mokheseng@gmail.com>
2026-04-30 20:36:30 +0200

    Connect frontend to deployed backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
-});
+  baseURL: "https://devblog-capstone-backend.onrender.com/api",
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
