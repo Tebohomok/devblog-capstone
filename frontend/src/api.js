5ddc0155066c8e682d5708380e930114f9c1bcca
Tebohomok <brateboho.mokheseng@gmail.com>
2026-04-30 20:57:41 +0200

    Fix frontend API config

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://devblog-capstone-backend.onrender.com/api",
+});
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
